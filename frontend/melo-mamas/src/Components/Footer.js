d88f7660864176627da2714af960492a02486924
Shani Cunnnigham <shanicunnnigham@MacBook-Air-8.local>
2020-08-22 15:22:56 -0400

    Fullstackshani link

--- frontend/melo-mamas/src/Components/Footer.js
+++ frontend/melo-mamas/src/Components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Footer () {
     return (
         <div className="footer">
-            <h1>The Melo Mama created by Shani Cunningham</h1>
+            <h1>The Melo Mama created by <a href="http://fullstackshani.com">FullStackShani.com</a></h1>
         </div>
     )
 }
