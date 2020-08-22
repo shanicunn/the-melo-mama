cc80f7ecc9f50c3be170b543cb0c9ff50800f43d
Shani Cunnnigham <shanicunnnigham@MacBook-Air-8.local>
2020-08-22 14:12:47 -0400

    Java/Spring files

---
+++ backend/src/main/java/com/melomamas/demo/MeloMamasApplication.java
@@ -0,0 +1,13 @@
+package com.melomamas.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeloMamasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeloMamasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/melomamas/demo/MeloMamasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.melomamas.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeloMamasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
