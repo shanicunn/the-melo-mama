737bc9f7d69069a7d08068c6064bcac82308dcf7
Shani Cunnnigham <shanicunnnigham@MacBook-Air-8.local>
2020-09-09 02:02:36 -0400

    Prices added to state

--- frontend/melo-mamas/src/Components/Services.js
+++ frontend/melo-mamas/src/Components/Services.js
@@ -1,83 +1,110 @@
 import React from 'react';
 
-function Services () {
-    return (
-        <main className="services">
-        {/* DOULA */}
-            <section>
-                <table>
-                    <thead> <th><h1>Doula Services </h1></th> <th><h1>Starting Costs</h1></th></thead>
-                    <tbody>
-                        <tr>
-                             <td>Antepartum</td>
-                             <td>$</td>
-                         </tr>
-                         <tr>
-                             <td>Birth</td>
-                             <td>$</td>
-                         </tr>
-                         <tr>
-                            <td>Postpartum</td>
-                            <td>$</td>
-                         </tr>
-                    </tbody>
-                </table>
-            </section>
-        {/* WRAP DEVICES */}
-            <section>
-                <table>
-                    <thead> <th><h1>Wrap Services </h1></th> <th><h1>Starting Costs</h1></th></thead>
-                    <tbody>
-                        <tr>
-                             <td>Belly Binds</td>
-                             <td>$</td>
-                         </tr>
-                         <tr>
-                             <td>Baby Wraps</td>
-                             <td>$</td>
-                         </tr>
+class Services extends React.Component {
+    constructor(props) {
+        super (props);
+        this.state = {
+            antepartum: 180,
+            birth: 180,
+            postpartum: 180,
 
-                    </tbody>
-                </table>
-            </section>
-        {/* PLACENTA */}
-                <table>
-                    <thead> <th><h1>Placenta Services </h1></th> <th><h1>Starting Costs</h1></th></thead>
-                    <tbody>
-                        <tr>
-                             <td>Placenta Encapsulation</td>
-                             <td>$</td>
-                         </tr>
-                         <tr>
-                             <td>Framed Placenta and Cord Keepsake</td>
-                             <td>$</td>
-                         </tr>
+            bellyBind: 30,
+            babyWrap: 30,
+            bindWithCloth: 110,
 
-                    </tbody>
-                </table>
-        {/* LACTATION */}
-                <table>
-                    <thead>
-                    <th><h1>Lactation Services </h1></th>
-                    <th><h1>Starting Costs</h1></th>
-                    </thead>
-                    <tbody>
-                        <tr>
-                             <td>Virtual</td>
-                             <td>$</td>
-                         </tr>
-                         <tr>
-                             <td>In Person</td>
-                             <td>$</td>
-                         </tr>
-                         <tr>
-                            <td>Lactation Cookies</td>
-                            <td>$</td>
-                         </tr>
-                    </tbody>
-                </table>
-        </main>
-    )
+            encapsulation: 250,
+            framedPlacenta: 270,
+
+            virtual: 225,
+            inPerson: 275,
+            cookies: 20,
+        }
+    }
+    render() {
+        return (
+            <main className="services">
+            {/* DOULA */}
+                <section>
+                    <table>
+                        <thead> <th><h1>Doula Services </h1></th> <th><h1>Starting Costs</h1></th></thead>
+                        <tbody>
+                            <tr>
+                                 <td>Antepartum</td>
+                                 <td>${this.state.antepartum}</td>
+                             </tr>
+                             <tr>
+                                 <td>Birth</td>
+                                 <td>${this.state.birth}</td>
+                             </tr>
+                             <tr>
+                                <td>Postpartum</td>
+                                <td>${this.state.postpartum}</td>
+                             </tr>
+                        </tbody>
+                    </table>
+                </section>
+            {/* WRAP DEVICES */}
+                <section>
+                    <table>
+                        <thead> <th><h1>Wrap Services </h1></th> <th><h1>Starting Costs</h1></th></thead>
+                        <tbody>
+                            <tr>
+                                 <td>Belly Binds</td>
+                                 <td>${this.state.bellyBind}</td>
+                             </tr>
+                             <tr>
+                                 <td>Baby Wraps</td>
+                                 <td>${this.state.babyWrap}</td>
+                             </tr>
+                             <tr>
+                                <td>Belly Binding With Cloth</td>
+                                <td>${this.state.bindWithCloth}</td>
+                             </tr>
+    
+                        </tbody>
+                    </table>
+                </section>
+            {/* PLACENTA */}
+                    <table>
+                        <thead> <th><h1>Placenta Services </h1></th> <th><h1>Starting Costs</h1></th></thead>
+                        <tbody>
+                            <tr>
+                                 <td>Placenta Encapsulation</td>
+                                 <td>${this.state.encapsulation}</td>
+                             </tr>
+                             <tr>
+                                 <td>Framed Placenta and Cord Keepsake</td>
+                                 <td>${this.state.framedPlacenta}</td>
+                             </tr>
+    
+                        </tbody>
+                    </table>
+            {/* LACTATION */}
+                    <table>
+                        <thead>
+                        <th><h1>Lactation Services </h1></th>
+                        <th><h1>Starting Costs</h1></th>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                 <td>Virtual</td>
+                                 <td>${this.state.virtual}</td>
+                             </tr>
+                             <tr>
+                                 <td>In Person</td>
+                                 <td>${this.state.inPerson}</td>
+                             </tr>
+                             <tr>
+                                <td>Lactation Cookies</td>
+                                <td>${this.state.cookies} per dozen</td>
+                             </tr>
+                        </tbody>
+                    </table>
+                    ***Domestic shipping costs start at $10
+            </main>
+        )
+    }
+    
 }
 
 
